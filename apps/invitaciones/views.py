64490f3424e86ca43e789d78a8f57dafb5060bbd
Kevin Giraud <kdgiraud88@gmail.com>
2023-02-26 01:02:43 -0300

    override main template

--- apps/invitaciones/views.py
+++ apps/invitaciones/views.py
@@ -3,5 +3,5 @@ from .models import invitado
 # Create your views here.
 
 def home(request):
-    invitados = invitado.all()
+    invitados = invitado.objects.all()
     return render(request,"main.html",{'invitados':invitados})

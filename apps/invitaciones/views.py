005624036a63dc62b961ffe8e89a16e7cd216e97
Kevin Giraud <kdgiraud88@gmail.com>
2023-08-09 20:08:46 -0300

    Backend insert completed

--- apps/invitaciones/views.py
+++ apps/invitaciones/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Invitado
 from .models import Tema
 # Create your views here.
@@ -8,3 +8,13 @@ def home(request):
     tema = Tema.objects.all()
     return render(request,"main.html",{'invitados':invitados})
     return render(request,"main.html",{'tema':tema})
+def addInvitados(request):
+    email = request.POST['floatingInputValueEmail']
+    fullName = request.POST['floatingInputValuefullName']
+    selfplus = request.POST['floatingInputValueNumber']
+    foodtype = request.POST['floatingInputValueFood']
+    song = request.POST['floatingTextareaSong']
+
+    invitado = Invitado.objects.create(email = email, nombre_completo = fullName, acompanantes = selfplus, tipo_comida = foodtype) 
+    tema = Tema.objects.create(texto = song, invitado = invitado)
+    return redirect('/')
